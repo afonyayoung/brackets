f5793d1bf579aaec5f6efc95f6b7008ff6948e81
afonyayoung <xafonya@gmail.com>
2019-03-08 16:35:40 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  if (str.length%2 !=0){
+    return false
+  }else {
+    var checked=[];
+    for (i=0; i<str.length; i++) {
+      for (j=0; j<bracketsConfig.length; j++){
+        if (str[i]==bracketsConfig[j][1]){
+        if (checked.length>0 && checked[checked.length-1]==bracketsConfig[j][0]) {
+            checked.pop();
+        } else {
+            checked.push(str[i]);
+        }           
+        } else {
+            if (str[i]==bracketsConfig[j][0]) {
+                checked.push(str[i]);
+            }
+        }
+      }  
+    }  
+    if (checked.length !==0){ 
+        return false;
+        } else {
+        return true;
+    } 
+  }  
 }
